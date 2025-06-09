bb873f46ef87f3cf493202ca1836e671503f416e
i-niranjan <niranjanchaudhari2004@gmail.com>
2025-06-09 19:13:41 +0530

    Frontend Intilisation:

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="text-2xl p-10 font-medium bg-amber-100">HELLO WORLD</div>
+    </>
+  );
+}
+
+export default App;
