a3327a9902f05fbb954e20739b859d81e21ca404
i-niranjan <niranjanchaudhari2004@gmail.com>
2025-07-22 23:00:33 +0530

    Finalised the initial template

--- frontend/src/models/blocks/components/PersonalInfoPreview.tsx
+++ frontend/src/models/blocks/components/PersonalInfoPreview.tsx
@@ -12,6 +12,7 @@ import {
 } from "@tabler/icons-react";
 import { Terminal } from "lucide-react";
 import { motion, useAnimationFrame } from "motion/react";
+import { Marquee } from "@/components/magicui/marquee";
 
 function PersonalInfoPreview() {
   const blocks = useSelector((state: RootState) => state.profile.blocks);
@@ -58,8 +59,8 @@ function PersonalInfoPreview() {
   // Assuming you want the first block's name
   return (
     <>
-      <div className="flex h-full w-full flex-col p-1 items-center gap-2">
-        <div className="flex flex-col gap-1 items-center w-full border-b pb-2">
+      <div className=" flex-1 h-full w-full flex-col p-1 items-center gap-2 ">
+        <div className="flex flex-col gap-1 items-center w-full pb-2">
           <div className="h-20 w-20 bg-accent rounded-full border overflow-hidden">
             <img
               src="https://github.com/shadcn.png"
@@ -88,7 +89,7 @@ function PersonalInfoPreview() {
         {/* About Section */}
 
         <div
-          className={` rounded-2xl px-3 py-4   shadow-2xl border overflow-hidden w-full`}
+          className={` rounded-2xl px-3 py-4    border overflow-hidden w-full`}
         >
           <div className="flex items-center justify-between mb-4">
             <div className="flex items-center space-x-2">
@@ -98,7 +99,7 @@ function PersonalInfoPreview() {
             </div>
             <div className="flex items-center space-x-2 text-gray-400 text-sm">
               <Terminal size={16} />
-              <span>hokori...</span>
+              <span>About Me...</span>
             </div>
           </div>
 
@@ -115,21 +116,46 @@ function PersonalInfoPreview() {
         </div>
 
         {/* Skills Section */}
-        <div className="relative whitespace-wrap w-full border overflow-hidden">
-          <div className="absolute top-0 left-0 z-10 h-full w-16 bg-gradient-to-r from-red to-transparent pointer-events-none" />
-          <motion.div
-            ref={ref}
-            className="mt-3 flex  items-center justify-center"
-          >
-            {repeatedSkills.map((item, index) => (
+        <div className="py-1 flex flex-col w-full overflow-hidden">
+          {/* <h3 className="text-sm font-semibold text-right">Skills</h3> */}
+          <Marquee className="[--duration:20s] ">
+            {skills.map((item, index) => (
               <div
                 key={index}
-                className="text-white shadow-xl  flex gap-2 border px-2 py-1 rounded-2xl bg-gradient-to-br bg-purple-800 via-purple-500 to-purple-700 items-center"
+                className="text-black   flex gap-1 border px-2 py-1 rounded-2xl  items-center"
               >
                 <item.icon /> &nbsp; <div>{item.name}</div>
               </div>
             ))}
-          </motion.div>
+          </Marquee>
+
+          <Marquee reverse className="[--duration:20s] ">
+            {skills.map((item, index) => (
+              <div
+                key={index}
+                className="text-black   flex gap-1 border px-2 py-1 rounded-2xl  items-center"
+              >
+                <item.icon /> &nbsp; <div>{item.name}</div>
+              </div>
+            ))}
+          </Marquee>
+        </div>
+
+        {/* USE- https://api.github.com/users/i-niranjan/repos */}
+        {/* GithubRepo */}
+
+        <div className="h-max mt-5 w-full flex flex-col gap-2">
+          <h2 className="font-semibold text-sm">Recent Projects</h2>
+          <div className="flex gap-2">
+            <div className="flex flex-col justify-between px-2 py-2 border relative  h-25 w-1/2 rounded-2xl overflow-hidden ">
+              <h3>Invoice Generator</h3>
+              <span>View More ...</span>
+            </div>
+            <div className="flex flex-col justify-between px-2 py-2 border relative  h-25 w-1/2 rounded-2xl overflow-hidden ">
+              <h3>Invoice Generator</h3>
+              <span>View More ...</span>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- frontend/src/models/dashboard/pages/dashboard.tsx
+++ frontend/src/models/dashboard/pages/dashboard.tsx
@@ -171,7 +171,7 @@ function Dashboard() {
             </div>
           </div>
           <div className="md:w-[40%] h-[35rem] px-4 py-2  flex items-center justify-center">
-            <div className="h-full w-[20rem] border-black border-3 rounded-4xl flex flex-col items-center p-2">
+            <div className="h-150 mt-20 w-[20rem] border-gray-200 border-2 rounded-4xl flex flex-col items-center p-2 ">
               <PersonalInfoPreview />
             </div>
           </div>
